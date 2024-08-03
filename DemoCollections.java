c981ae12ee1e481313bd01f5224cded5aaef99e5
jesushedd <jesushe99@gmail.com>
2024-08-03 12:07:22 -0500

    using maps(dictionaries) and list built in java

--- DemoCollections.java
+++ DemoCollections.java
@@ -27,11 +27,46 @@ public class DemoCollections {
         for (String ithWord : words) {
             uniqueWords.add(ithWord);
         }
+
         return uniqueWords.size();
+    }
+
+    /*
+    * Returns  a map that tracks the count of all specified target words in words(main list) */
+    public static Map<String, Integer> collectWordCount(List<String> wordList, List<String> targetWords){
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        //Make a note that we have seen none of thw words
+        for (String t : targetWords){
+            counts.put(t, 0);
+        }
+
+        //Start finding and add +1 if found
+        for (String w : wordList){
+            //int oldCount = counts.get(w);
+            //counts.computeIfPresent(w, "");
+            if (counts.containsKey(w)){
+                int oldCount  = counts.get(w);
+                counts.put(w, oldCount + 1);
+            }
+        }
+        return counts;
     }
 
     public static void main(String[] args) {
         List<String> w = getWords("lotteryOfBabylon.txt");
-        System.out.println(countUniqueWord(w));
+        //System.out.println(countUniqueWord(w));
+        Set<String> wordSet = new HashSet<>(w);
+        System.out.println(wordSet.size());
+
+        //Target word to count in w
+        List<String> targets = new ArrayList<>();
+        targets.add("lottery");
+        targets.add("the");
+        targets.add("babylon");
+
+        Map<String, Integer> targetWordsInListCount = collectWordCount(w, targets);
+        System.out.println(targetWordsInListCount);
+
+
     }
 }
